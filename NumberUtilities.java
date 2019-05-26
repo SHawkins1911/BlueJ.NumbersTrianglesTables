b73746ace645c0cc276cc7777beb500663320dd6
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-05-25 23:06:54 -0400

    first push

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,79 @@
 
 
-
+import static java.lang.Math.*;
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+       String outcome = "";
+        for(int i = 0; i < stop; i++){
+         outcome += Integer.toString(i);
+        }
+        
+        System.out.println(outcome);
+        return outcome;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String outcome = "";
+        for(int i = start; i < stop; i++){
+         outcome += Integer.toString(i);
+        }
+        
+        System.out.println(outcome);
+        return outcome;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String outcome = "";
+        for(int i = start; i < stop; i+=step){
+         outcome += Integer.toString(i);
+        }
+        
+        System.out.println(outcome);
+        return outcome;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String outcome = "";
+        
+        for(int i = start; i < stop; i++){
+             if(i % 2 == 0){
+            
+            outcome += Integer.toString(i);
+        
+    };
+    }
+        System.out.println(outcome);
+        return outcome;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+          String outcome = "";
+        
+        for(int i = start; i < stop; i++){
+             if(i % 2 == 1){
+            
+            outcome += Integer.toString(i);
+        
+    };
+    }
+        System.out.println(outcome);
+        return outcome;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+         String outcome = "";
+          double i;
+            for( i = start; i < stop; pow(i, exponent))
+            {
+                
+                outcome += Double.toString(i);
+        
+            };
+            System.out.println(outcome);
+            return outcome;
     }
-}
+    }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,37 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        
+        int width = numberOfStars;
+        String outcome = "";
+        StringBuilder result = new StringBuilder();
+               for(int i = 0; i < width; i++){
+                   
+                        result.append("*");
+                                    
+        }
+                        outcome = result.toString();
+                        System.out.println(outcome);
+                            return outcome;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+      StringBuilder result = new StringBuilder();
+      String outcome = "";
+          for (int i = 0; i < numberOfRows; i++){
+    
+               result.append("*");
+               outcome = result.toString();         
+               System.out.print(outcome + "\n");
+                  }   
+            return outcome;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
-    }
-
+     return getTriangle(4) ;
+}
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
