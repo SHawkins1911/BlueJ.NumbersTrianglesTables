be58c22140389fde175f2151056a4f1ca5281e9b
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-05-27 22:44:38 -0400

    Finished

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,50 @@
- 
+
+ import java.util.Formatter;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String outcome = "";      
+              for(int i = 1; i <= 5; i++){
+               
+                  for(int x = 1; x <= 5; x++){
+                      
+                      int output = (x * i);
+                    
+                      outcome += String.format("%3d |", output);
+                    }
+                    outcome += "\n";
+                    
+                } return outcome;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String outcome = "";      
+              for(int i = 1; i <= 10; i++){
+               
+                  for(int x = 1; x <= 10; x++){
+                      
+                      int output = (x * i);
+                    
+                      outcome += String.format("%3d |", output);
+                    }
+                    outcome += "\n";
+                    
+                } return outcome;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
-    }
-}
+      
+      String outcome = "";      
+              for(int i = 1; i <= tableSize; i++){
+               
+                  for(int x = 1; x <= tableSize; x++){
+                      
+                      int output = (x * i);
+                    
+                      outcome += String.format("%3d |", output);
+                    }
+                    outcome += "\n";
+                    
+                } return outcome;
+}  
+    }   
